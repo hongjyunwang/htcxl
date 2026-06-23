b9ce417696fe9908c3753171a0f41192d6e7e19e
Hong-Jyun Wang <hongjyunwa@gmail.com>
2026-06-23 15:43:41 +0800

    sram module

---
+++ src/sram.sv
@@ -0,0 +1,32 @@
+// Basic SRAM module
+// Each SRAM holds one way across all sets
+
+module sram  #(
+    parameter int DATA_W = 64, // 1 + TAG_W + NUM_NODES + NUM_NODES, set by wrapper
+    parameter int DEPTH = 8 // depth = number of sets
+)(
+    input logic clk_i,
+
+    // Write data input
+    input logic ce_i,
+    input logic we_i,
+    input logic [$clog2(DEPTH)-1:0] idx_i,
+    input logic [DATA_W-1:0] wdata_i,
+    
+    // Read data output 
+    output logic [DATA_W-1:0] rdata_o
+);
+
+    logic [DATA_W-1:0] mem [0:DEPTH-1];
+
+    // rw takes one cycle each
+    always_ff @(posedge clk_i) begin
+        if (ce_i) begin
+            if (we_i)
+                mem[idx_i] <= wdata_i;
+            else
+                rdata_o <= mem[idx_i];
+        end
+    end
+
+endmodule
